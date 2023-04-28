b7da58d0562ce9000dfd708467e3d173cb4353de
LeoTulini <tulini.leo@hotmail.com>
2023-04-28 12:43:39 -0300

    Passar validações para classe Funcionario

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,5 @@
 
 9
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+]
+-src/main/test/FuncionarioAssalariadoTest.java,c\e\ce70ea00b0f147dfdaf9f3ee22230b5947037d07
--- src/main/java/funcionarios/Funcionario.java
+++ src/main/java/funcionarios/Funcionario.java
@@ -41,6 +41,7 @@ public abstract class Funcionario {
     }
 
     public void setNome(String nome) {
+        validarNome(nome);
         this.nome = nome;
     }
 
@@ -57,6 +58,7 @@ public abstract class Funcionario {
     }
 
     public void setTaxaComissao(double taxaComissao) {
+        validarTaxaComissao(taxaComissao);
         this.taxaComissao = taxaComissao;
     }
 
@@ -65,6 +67,7 @@ public abstract class Funcionario {
     }
 
     public void setVendasBrutas(double vendasBrutas) {
+        validarVendasBrutas(vendasBrutas);
         this.vendasBrutas = vendasBrutas;
     }
 
@@ -73,10 +76,70 @@ public abstract class Funcionario {
     }
 
     public void setSalario(Double salario) {
+        validarSalario(salario);
         this.salario = salario;
     }
 
     public void setHorasTrabalhadas(Double horasTrabalhadas) {
+        validarHorasTrabalhadas(horasTrabalhadas);
         this.horasTrabalhadas = horasTrabalhadas;
+    }
+
+    private void validarNome(String nome) {
+        if (nome == null || nome.trim().length() < 3) {
+            throw new IllegalArgumentException("O nome não pode ser vazio e deve ter no mínimo 3 caracteres.");
+        }
+    }
+
+    private void validarTaxaComissao(double taxaComissao) {
+        if (taxaComissao < 0 || taxaComissao > 100) {
+            throw new IllegalArgumentException("A taxa de comissão deve estar entre 0 e 100.");
+        }
+    }
+
+    private void validarVendasBrutas(double vendasBrutas) {
+        if (vendasBrutas < 0 || vendasBrutas > 100) {
+            throw new IllegalArgumentException("As vendas brutas devem estar entre 0 e 100.");
+        }
+    }
+
+    private void validarSalario(Double salario) {
+        if (salario < 0) {
+            throw new IllegalArgumentException("O salário não pode ser negativo.");
+        }
+    }
+
+    private void validarHorasTrabalhadas(Double horasTrabalhadas) {
+        if (horasTrabalhadas < 0) {
+            throw new IllegalArgumentException("As horas trabalhadas não podem ser negativas.");
+        }
+    }
+
+    private void validarCpf(String cpf) {
+
+        String cpf_validar = cpf.substring(0, cpf.length() - 2);
+        int reverso = 10;
+        int total = 0;
+        for (int index = 0; index < 19; index++) {
+            if (index > 8) {
+                index -= 9;
+            }
+            total += (int)(cpf_validar.charAt(index)) * reverso;
+            reverso--;
+            if (reverso < 2) {
+                reverso = 11;
+                int d = 11 - (total % 11);
+                if (d > 9) {
+                    d = 0;
+                }
+                total = 0;
+                cpf_validar += d;
+            }
+        }
+        boolean sequencia = cpf_validar.equals(cpf_validar.charAt(0) + "".repeat(Math.max(0, cpf.length() - 2)));
+        if (cpf.equals(cpf_validar) && !sequencia) {
+            return;
+        }
+        throw new IllegalArgumentException ("Cpf inválido.");
     }
 }
